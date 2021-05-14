e5aacdab54233ec7d1efa224498b0303895e34e2
全 <1583090876@qq.com>
2021-05-14 10:54:10 +0800

    question 28

---
+++ src/cn/lilq/question28/Solution.java
@@ -0,0 +1,11 @@
+package cn.lilq.question28;
+
+/**
+ * @auther: Li Liangquan
+ * @date: 2021/5/14 10:51
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
