aafe5c3c22b96766f0b92e9ea081b004fa677862
daquan <1583090876@qq.com>
2021-06-15 18:42:36 +0800

    question69

--- src/cn/lilq/Test.java
+++ src/cn/lilq/Test.java
@@ -2,33 +2,55 @@ package cn.lilq;
 
 
 
-import cn.lilq.question22.Solution;
-import cn.lilq.question22.Solution2;
-
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @auther: Li Liangquan
  * @date: 2021/5/10 19:38
  */
 public class Test {
-    private List<List<Integer>> lists;
-
-    void test1(){
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        lists.add(list);
+    private static boolean repetition(List<Integer> list){
+        int temp;
+        for (int i = 0; i < list.size(); i++) {
+             temp = list.remove(i);
+            if (list.contains(temp)){
+                return true;
+            }
+            list.add(i,temp);
+        }
+        return false;
     }
 
-    void tesr(){
-        lists = new ArrayList<>();
-        test1();
+    private static List<Integer> addI(List<Integer> list){
+        List<Integer> listNew = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            listNew.add(list.get(i)+i+1);
+        }
+        return listNew;
     }
 
     public static void main(String[] args) {
-        Test test = new Test();
-        test.tesr();
-        System.out.println(test.lists);
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        String abc = "abc";
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(scanner.nextInt());
+        }
+        int pathSize = 0;
+        while (true){
+            if (repetition(list)){
+                System.out.println(pathSize);
+                break;
+            }
+            list = addI(list);
+            pathSize++;
+        }
+
     }
+
+
 }
